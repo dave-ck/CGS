a1298a6ef9cb7fc0574e9f4b0d3c42d4ee8c4849
dave-ck <daveck@protonmail.com>
2019-03-13 21:16:55 +0000

    stls added

--- stlScanner.js
+++ stlScanner.js
@@ -20,10 +20,10 @@ function lookupFacetProcess(facetString, index) {
     let normal = facet.slice(i_normal + 1, i_normal + 4).map(parseFloat);
     let i_v1 = facet.indexOf("vertex");
     let v1 = facet.slice(i_v1 + 1, i_v1 + 4).map(parseFloat);
-    facet = facet.slice(i_v1+4);
+    facet = facet.slice(i_v1 + 4);
     let i_v2 = facet.indexOf("vertex");
     let v2 = facet.slice(i_v2 + 1, i_v2 + 4).map(parseFloat);
-    facet = facet.slice(i_v2+4);
+    facet = facet.slice(i_v2 + 4);
     let i_v3 = facet.indexOf("vertex");
     let v3 = facet.slice(i_v3 + 1, i_v3 + 4).map(parseFloat);
     let vertices = v1.concat(v2).concat(v3);
@@ -33,7 +33,7 @@ function lookupFacetProcess(facetString, index) {
     };
 }
 
-function devSTL(source_path){
+function devSTL(source_path) {
     let data = fs.readFileSync(source_path, 'utf-8');
     let vertices = [];
     let normals = [];
@@ -81,26 +81,36 @@ function readSTL(source_path) {
 
 
 // generate a "dummy" 3D model with
-function genDummy(size){
+function genDummy(size) {
     let vertices = [];
     let indices = [];       //completely redundant in this case, can be optimized if rendering per-frame becomes slow
-    for (let i=0.0; i<size; i++){
-        vertices = vertices.concat([i, i, i+1, i, i+1, i, i+1, i, i]);
-        indices.push(i*3);
-        indices.push(i*3+1);
-        indices.push(i*3+2);
+    for (let i = 0.0; i < size; i++) {
+        vertices = vertices.concat([i, i, i + 1, i, i + 1, i, i + 1, i, i]);
+        indices.push(i * 3);
+        indices.push(i * 3 + 1);
+        indices.push(i * 3 + 2);
     }
     // normals may be wrong, but aren't causing current bug (not used in present rendering)
-    return {vertices:vertices, indices: indices};
+    return {vertices: vertices, indices: indices};
 }
 
 let models = JSON.parse(fs.readFileSync("./models.json", 'utf-8'));
 
 // INSERT CODE TO ADD TO MODELS HERE i.e.
-models.pointy = readSTL("./STL_Sources/pointy.stl");    // overwrites existing models.pointy
+//models.branches50 = readSTL("./STL_Sources/branches50.stl");    // overwrites existing models.pointy
 
+let newModels = {
+    branches50: models.branches50,
+    leaves50: models.leaves50,
+    slopes: readSTL("./STL_Sources/slopes.stl"),
+    riverBank: readSTL("./STL_Sources/riverBank.stl"),
+    river: readSTL("./STL_Sources/river.stl"),
+    footPath: readSTL("./STL_Sources/footPath.stl"),
+    flower: readSTL("./STL_Sources/flower.stl"),
+    pointy: readSTL("./STL_Sources/pointy.stl"),
+};
 
-fs.writeFile("./models.json", JSON.stringify(models), null, 2);
+fs.writeFile("./models.json", JSON.stringify(newModels), null, 2);
 
 
 console.log("\nDone loading models. models.json has been updated.");
